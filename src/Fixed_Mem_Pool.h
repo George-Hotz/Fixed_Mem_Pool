498c0c93b204431bde330cd782dccd821cc44f9a
雪花 <61445565+George-Hotz@users.noreply.github.com>
2024-05-27 20:53:46 +0800

    Update Fixed_Mem_Pool.h

--- src/Fixed_Mem_Pool.h
+++ src/Fixed_Mem_Pool.h
@@ -15,16 +15,15 @@ public:
     Fixed_Mem_Pool(size_t psize, size_t objsize);
     ~Fixed_Mem_Pool();
 
-    struct Chunk{                  
-        Chunk *next;
-    };
-
     void* palloc();
     void pfree(void *obj);
-    
+
+private:
+    void*& objNext(void* obj);
+
 private:
     void *_memory = nullptr;       // 申请的过量内存块
-    Chunk *_freelist = nullptr;    // 空闲内存块链表
+    void *_freelist = nullptr;    // 空闲内存块链表
     size_t _remainBytes = 0;       // 内存池内存剩余的字节
     size_t _objSize = 0;           // 定长类型的大小
     size_t _pool_size;             // 内存池的内存大小
